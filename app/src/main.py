9549900ab6d3e28fbf44c960eeb836831f2062ef
Caccavale <samcaccavale@gmail.com>
2019-02-15 11:42:28 -0500

    Added JWT

--- app/src/main.py
+++ app/src/main.py
@@ -2,9 +2,10 @@ import urllib
 from base64 import b64encode
 from uuid import uuid4
 
-import redis
+import jwt
 import requests
 import uvicorn
+from redis import Redis
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 
@@ -14,18 +15,15 @@ with open("secrets/client_id.secret", "r") as f:
     CLIENT_ID = f.read().rstrip("\n")
 with open("secrets/client_secret.secret", "r") as f:
     CLIENT_SECRET = f.read().rstrip("\n")
+with open("secrets/jwt.secret", "r") as f:
+    JWT_SECRET = f.read().rstrip("\n")
 
 AUTH_URL = "https://www.bungie.net/en/oauth/authorize"
 
 app = Starlette()
 
 
-r = redis.Redis(host="db", port=6379, db=0)
-
-
-def generate_oauth_url_with_tokens():
-    # TODO: In the future we should include a JWT CSRF token
-    return AUTH_URL
+r = Redis(host="db", port=6379, db=0)
 
 
 @app.route("/")
@@ -33,31 +31,16 @@ async def index(request):
     return JSONResponse({"mes": "Hello world!"})
 
 
-@app.route("/register")
-async def register(request):
-    user = request.query_params.get("u")
-    if user is None:
-        return "Invalid registration link."
-    else:
-        if r.get(user) is not None:
-            print("User already registered")
-            # Todo handle this
-    state = str(uuid4())
-    # session["state"] = state
-    # session["user"] = user
-    return JSONResponse(
-        AUTH_URL
-        + "?"
-        + urllib.parse.urlencode(
-            {"state": state, "client_id": CLIENT_ID, "response_type": "code"}
-        )
-    )
-
-
 @app.route("/callback/bungie", methods=["GET", "POST"])
 async def bungie_redirect(request):
-    # TODO: In the future we should check the included JWT CSRF token
+    state = request.query_params.get("state")
+    if state is None:
+        return JSONResponse("No state found")
+
+    token = jwt.decode(state, JWT_SECRET, algorithms=["HS256"])
+
     authorization_code = request.query_params.get("code")
+
     r = requests.post(
         "https://www.bungie.net/platform/app/oauth/token/",
         data={"grant_type": "authorization_code", "code": authorization_code},
@@ -69,8 +52,6 @@ async def bungie_redirect(request):
             ),
         },
     )  # TODO: This should be abstracted over
-    return JSONResponse(r.json())
 
-
-# if __name__ == "__main__":
-#     uvicorn.run(app, host='0.0.0.0', port=80)
+    # r.set()
+    return JSONResponse({token.get("user"): r.json()})
--- bot/src/event_handlers.py
+++ bot/src/event_handlers.py
@@ -1,8 +1,17 @@
+import jwt
+
+with open("jwt.secret", "r") as f:
+    JWT_SECRET = f.read().rstrip("\n")
+
+
 async def ping(client, message):
     await message.channel.send("Pong!")
 
 
 async def register(client, message):
     await message.channel.send(
-        f"Register at https://ada-2.cc/register?u={message.author.id}"
+        "I've sent registration instructions to you _privately_."
+    )
+    await message.author.send(
+        f"Register at https://www.bungie.net/en/oauth/authorize?client_id=26007&response_type=code&state={ jwt.encode({'user': message.author.id}, JWT_SECRET, algorithm='HS256').decode('utf-8') }"
     )
